936e17f4507ff9bace77adfce3c7bcc901346aeb
HDragon <houyonghua2015@gmail.com>
2023-03-09 15:38:40 +0800

    Update main.sh

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -16,6 +16,7 @@ function mvdir() {
 mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
+git_clone https://github.com/HDragon8/noodles-default-settings noodles-default-settings
 git clone --depth 1 https://github.com/kenzo78/my-packages && mvdir my-packages
 git clone --depth 1 https://github.com/kiddin9/luci-app-dnsfilter
 git clone --depth 1 https://github.com/kiddin9/aria2
